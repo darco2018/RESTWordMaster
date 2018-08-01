daf6fa681846f499fc4ee6d3eb0c91f3ce8b22f3
darco2018﻿ <us3cki@zoho.com>
2018-08-01 19:00:25 +0200

    Initial commit

---
+++ src/main/java/com/wordmaster/RestWordMasterApplication.java
@@ -0,0 +1,12 @@
+package com.wordmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWordMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWordMasterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wordmaster/RestWordMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wordmaster;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestWordMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
